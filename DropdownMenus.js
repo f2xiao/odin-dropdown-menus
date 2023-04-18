d7e0be62c0098c4f00f678e325ebe93a694efb9f
f2xiao <25191617+f2xiao@users.noreply.github.com>
2023-04-18 16:09:32 -0400

    Add component DropdownMenus

--- DropdownMenus.js
+++ DropdownMenus.js
@@ -1,22 +1,43 @@
-export default function(){
-    // Select the button Element and the dropdown-content element 
-    const btnEle = document.querySelectorAll('.dropdown-btn');
-    const dropdownContEle = document.querySelectorAll('.dropdown-content');
-    
+const template = document.createElement('template');
+template.innerHTML = `
+<div class="dropdown-container">
+    <button class="dropdown-btn">Click Me</button>
+    <ul class="dropdown-content">
+        <li class="dropdown-item"><a href="#">link 1</a></li>
+        <li class="dropdown-item"><a href="#">link 2</a></li>
+        <li class="dropdown-item"><a href="#">link 3</a></li>
+    </ul> 
+</div>
+`
 
-    // register event handler on hover:
-    // show the dropdown-content 
-
-    btnEle.forEach((btn,index) => { 
-        
-        btn.addEventListener('mouseover', () => { 
-            dropdownContEle[index].classList.toggle('visible')
-        });
-
-        btn.addEventListener('mouseout', () => { 
-            dropdownContEle[index].classList.toggle('visible')
-        })
-        
-    })
+const style = document.createElement('style');
+style.textContent = `
+/* dropdown content (hidden by default) */
+.dropdown-content{
+   display: none;
 }
+/* show the dropdown-content on hover */
+.visible{
+    display: block;
+}`
 
+class DropdownMenus extends HTMLElement {
+    constructor(){
+        super();
+
+        const shadow = this.attachShadow({mode: 'open'});
+        shadow.appendChild(style);
+        shadow.appendChild(template.content.cloneNode(true));
+
+        const btnEle = shadow.querySelector('.dropdown-btn');
+        const dropdownContEle = shadow.querySelector('.dropdown-content');
+
+        btnEle.addEventListener('mouseover', () => { 
+            dropdownContEle.classList.add('visible')
+         })
+
+        btnEle.addEventListener('mouseout', () => { 
+            dropdownContEle.classList.add('visible')
+         })
+    }
+}
