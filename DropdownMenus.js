8b512142d3fc6503a537cc85064ca5281deca07b
arashi4ever <25191617+f2xiao@users.noreply.github.com>
2023-04-20 14:16:09 -0400

    Fix bug

--- DropdownMenus.js
+++ DropdownMenus.js
@@ -1,90 +1,92 @@
-const template = document.createElement('template');
-template.innerHTML = `
-<link rel="stylesheet" href="styles.css">
-<div class="dropdown-container">
-    <button class="dropdown-btn"><slot name="name">Click me</slot></button>
-    <ul class="dropdown-content">
-        <li class="dropdown-item"><a href="#"><slot name="link1">Link 1</slot></a></li>
-        <li class="dropdown-item"><a href="#"><slot name="link2">Link 2</slot></a></li>
-        <li class="dropdown-item"><a href="#"><slot name="link3">Link 3</slot></a></li>
-    </ul> 
-</div>
-`
-
-const style = document.createElement('style');
-style.textContent = `
-.dropdown-container{
-    display:flex;
-    flex-direction: column;
-    width: 8rem;
-    margin: 1rem 1rem 0;
-    font-size: 1rem;
-    align-items: start;
-    justify-content: start;
-  }
-
-  .dropdown-btn {
-    display: inline-block;
-
-    width: 100%;
-    padding: 10px 0;
-    padding-left: 1rem;
-    border-radius: 5px;
-    background-color: #666;
-
-    text-align: start;
-    color: white;
-
-    cursor: pointer;
-  }
-
-  .dropdown-btn:hover, 
-  .dropdown-btn:active {
-    background-color: #ccc;
-  }
-
-  .dropdown-item{
-    padding: 10px 0;
-    border-radius: 5px;
-  }
-
-  .dropdown-item:hover{
-    background-color: #999;
-  }
-
-  .dropdown-item > a:link {
-    padding-left: 1rem;
-
-    color: #666;
-    text-decoration: underline;
-  }
-
-  .dropdown-item > a:active  {
-    color: white;
-  }
-
-/* dropdown content (hidden by default) */
-.dropdown-content{
-    display: none;
-    width: 100%;
-   
-    font-size: 1rem;
-}
-
-
-/* show the dropdown-content on hover */
-.visible{
-    display: block;
-}`
-
 export default class DropdownMenus extends HTMLElement {
     constructor(){
         super();
 
+        const template = document.createElement('template');
+        template.innerHTML = `
+        <link rel="stylesheet" href="styles.css">
+        <div class="dropdown-container">
+            <button class="dropdown-btn"><slot name="name">Hover me</slot></button>
+            <ul class="dropdown-content">
+                <li class="dropdown-item"><a href="#"><slot name="link1">Link 1</slot></a></li>
+                <li class="dropdown-item"><a href="#"><slot name="link2">Link 2</slot></a></li>
+                <li class="dropdown-item"><a href="#"><slot name="link3">Link 3</slot></a></li>
+            </ul> 
+        </div>
+        `
+
+        const style = document.createElement('style');
+        style.textContent = `
+        .dropdown-container{
+            display:flex;
+            flex-direction: column;
+            width: 8rem;
+            margin: 1rem 1rem 0;
+            font-size: 1rem;
+            align-items: start;
+            justify-content: start;
+          }
+
+          .dropdown-btn {
+            display: inline-block;
+
+            width: 100%;
+            padding: 10px 0;
+            padding-left: 1rem;
+            border-radius: 5px;
+            background-color: #666;
+
+            text-align: start;
+            color: white;
+
+            cursor: pointer;
+          }
+
+          .dropdown-btn:hover, 
+          .dropdown-btn:active {
+            background-color: #ccc;
+          }
+
+          .dropdown-item{
+            padding: 10px 0;
+            border-radius: 5px;
+          }
+
+          .dropdown-item:hover{
+            background-color: #999;
+          }
+
+          .dropdown-item > a:link {
+            padding-left: 1rem;
+
+            color: #666;
+            text-decoration: underline;
+          }
+
+          .dropdown-item > a:active  {
+            color: white;
+          }
+
+        /* dropdown content (hidden by default) */
+        .dropdown-content{
+            display: none;
+            width: 100%;
+          
+            font-size: 1rem;
+        }
+
+
+        /* show the dropdown-content on hover */
+        .visible{
+            display: block;
+        }`
+
         const shadow = this.attachShadow({mode: 'open'});
+        
         shadow.appendChild(style);
         shadow.appendChild(template.content.cloneNode(true));
         this.style.display = 'inline-block';
+        this.style.width = '8rem';
 
         const dropdownContEle = shadow.querySelector('.dropdown-content');
 
@@ -118,3 +120,5 @@ export default class DropdownMenus extends HTMLElement {
         
     }
 }
+
+customElements.define('dropdown-menus', DropdownMenus);
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 import DropdownMenus from './DropdownMenus.js'
 
-customElements.define('dropdown-menus', DropdownMenus)
+
+
